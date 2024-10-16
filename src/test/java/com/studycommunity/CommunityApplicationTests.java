cc850679374341f1845cc12f241a724b37e46bb4
chandler <parksm0529@gmail.com>
2024-10-16 21:29:14 +0900

    create new project

---
+++ src/test/java/com/studycommunity/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
