462928b84d175e9e3a72ff3cf76550cded43ef9c
chandler <parksm0529@gmail.com>
2024-10-19 11:35:54 +0900

    refactor) apply @InitBinder to initialize a validator before binding data

--- src/main/java/com/studycommunity/account/AccountController.java
+++ src/main/java/com/studycommunity/account/AccountController.java
@@ -6,7 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -16,6 +18,11 @@ public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
 
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.addValidators(signUpFormValidator);
+    }
+
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
         model.addAttribute(new SignUpForm());
@@ -24,11 +31,6 @@ public class AccountController {
 
     @PostMapping("/sign-up")
     public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
-        if (errors.hasErrors()) {
-            return "account/sign-up";
-        }
-
-        signUpFormValidator.validate(signUpForm, errors);
         if (errors.hasErrors()) {
             return "account/sign-up";
         }
